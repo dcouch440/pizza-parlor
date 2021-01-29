c3740130f7292b6035b2373a228b7ca1c776e7f8
dcouch440 <dcouch440@gmail.com>
2021-01-29 11:29:45 -0800

    PASS menu.setSize('small')

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 import {Menu} from './classes/Menu/_Menu.js'
 
 let menu = new Menu()
-menu.setSize('medium')
+menu.setSize('small')
 console.log(menu)
 
 document.addEventListener('DOMContentLoaded', () => {})
