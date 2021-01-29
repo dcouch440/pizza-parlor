036a8de4050918512435e1a84316e953372a8cae
dcouch440 <dcouch440@gmail.com>
2021-01-29 13:19:16 -0800

    add buttons to list

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,29 @@
 import {Menu} from './classes/Menu/_Menu.js'
 
-let menu = new Menu()
-menu.setPaggiaro()
-menu.setPaggiaro()
-menu.setPaggiaro()
-menu.setPaggiaro()
-console.log(menu)
+const menuComponent = (obj) => {
+  return `
+    <div id="menuComponent">
+      <div id="menu-name">${obj[0]} -${obj[1]}${obj[2]}</div>
+      <div id="menu-description">${obj[3]}</div>
+      <button id="${obj[6]}">Add ${obj[0]}</button>
+    </div>
+  `
+}
 
-document.addEventListener('DOMContentLoaded', () => {})
+document.addEventListener('DOMContentLoaded', () => {
+  let menu = new Menu()
+  const myMenuItems = [
+    menu.parmigiano, 
+    menu.paggiaro,
+    menu.brie,
+    menu.onion,
+    menu.olive,
+    menu.tomato,
+    menu.mushroom
+  ]
+  const menuSelector = document.getElementById('menu-selector') 
+  const menuMapper = myMenuItems.map(item => {
+    return menuComponent(item)
+  }).join('')
+  menuSelector.innerHTML = menuMapper
+})
