66b7702fcd9ee1a1d75cb9283fc7da55355ef1bf
dcouch440 <dcouch440@gmail.com>
2021-01-29 08:20:40 -0800

    add sizes contructor

---
+++ js/classes/Sizes/_Sizes.js
@@ -0,0 +1,9 @@
+import './getSizes'
+import './setSizes'
+
+function Menu() {
+  this.sizes = {};
+  this.toppings = {};
+  this.crust = {};
+  this.cheese = {};
+}
---
+++ js/classes/Sizes/setSizes.js
@@ -0,0 +1,1 @@
+import {Sizes} from './Sizes.js'
