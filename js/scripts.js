e746131b0e62e7c5b3f69a9a17bcba1611622009
dcouch440 <dcouch440@gmail.com>
2021-01-29 11:25:41 -0800

    PASS setTomatoPrice()

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 import {Menu} from './classes/Menu/_Menu.js'
 
 let menu = new Menu()
-menu.setOlivePrice()
-menu.setOlivePrice()
-menu.setOlivePrice()
-menu.setOlivePrice()
+menu.setTomatoPrice()
+menu.setTomatoPrice()
+menu.setTomatoPrice()
+menu.setTomatoPrice()
 console.log(menu)
 
 document.addEventListener('DOMContentLoaded', () => {})
