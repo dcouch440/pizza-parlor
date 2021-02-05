a997bd468932685d92c4d7cf2230a7e8bb58efe2
dcouch440 <dcouch440@gmail.com>
2021-02-04 18:01:46 -0800

    simplify code

--- js/classes/Menu.js
+++ js/classes/Menu.js
@@ -1,70 +1,35 @@
 export function Menu() {
-  this.parmigiano = ['Parmigiano', '$', 5, 'Sharper than your grandads straight razor!', 0, {total: 0}, 'parmigiano-button'];
-  this.paggiaro = ['Paggiaro', '$', 3, 'A robust classic.', 0, {total: 0}, 'paggiaro-button'];
-  this.brie = ['Brie', '$', 2, 'A soft farmhouse Cheese. Carful it\'s addicting.', 0, {total: 0}, 'brie-button'];
-  this.onion = ['Onion', '$', 1, 'A gift from earth.', 0, {total: 0}, 'onion-button'];
-  this.olive = ['Olive', '$', 1, 'Pungent.. nuff said.', 0, {total: 0}, 'olive-button'];
-  this.tomato = ['Tomato', '$', 1, 'Slightly sweet, never sour.', 0, {total: 0}, 'tomato-button'];
-  this.mushroom = ['Mushroom', '$', 2, 'These mushrooms are the best around!', 0, {total: 0}, 'mushroom-button'];
-  this.size = 'medium';
-  this.small = 2;
-  this.medium = 4;
-  this.large = 5;
-  this.totalCost = 0;
+  this.parmigiano = 0;
+  this.paggiaro = 0;
+  this.brie = 0;
+  this.onion = 0;
+  this.olive = 0;
+  this.tomato = 0;
+  this.mushroom = 0;
+  this.size = undefined;
 };
-const updateNodes = (currentNode, objKey) => {
-  currentNode.innerHTML = objKey[5].total;
-};
-const threeMax = (objKey, thisArg, currentNode) => {
-  objKey[4] += 1;
-  if (objKey[4] < 4){ 
-    objKey[5].total += 1;
-    thisArg.totalCost = thisArg.totalCost + objKey[2];
-  };
-  updateNodes(currentNode, objKey);
-};
-Menu.prototype.setParmigianoPrice = function(currentNode) {
-  threeMax(this.parmigiano, this, currentNode);
-};
-Menu.prototype.setPaggiaroPrice = function(currentNode) {
-  threeMax(this.paggiaro, this, currentNode);
-};
-Menu.prototype.setBriePrice = function(currentNode) {
-  threeMax(this.brie, this, currentNode);
-};
-Menu.prototype.setOnionPrice = function(currentNode) {
-  threeMax(this.onion, this, currentNode);
-};
-Menu.prototype.setOlivePrice = function(currentNode) {
-  threeMax(this.olive, this, currentNode);
-};
-Menu.prototype.setTomatoPrice = function(currentNode) {
-  threeMax(this.tomato, this, currentNode);
-};
-Menu.prototype.setMushroomPrice = function(currentNode) {
-  threeMax(this.mushroom, this, currentNode);
-};
+
+Menu.prototype.setParmigianoPrice = function() {
+  this.parmigiano += 1
+}
+Menu.prototype.setPaggiaroPrice = function() {
+  this.parmigiano += 1
+}
+Menu.prototype.setBriePrice = function() {
+  this.parmigiano += 1
+}
+Menu.prototype.setOnionPrice = function() {
+  this.parmigiano += 1
+}
+Menu.prototype.setOlivePrice = function() {
+  this.parmigiano += 1
+}
+Menu.prototype.setTomatoPrice = function() {
+  this.parmigiano += 1
+}
+Menu.prototype.setMushroomPrice = function() {
+  this.parmigiano += 1
+}
 Menu.prototype.setSize = function(size) {
   this.size = size;
-};
-Menu.prototype.displayCurrentPrice = function() {
-  let cost = 0;
-  switch(this.size) {
-    case ('small'):
-      cost = this.totalCost + this.small;
-      break;
-    case ('medium'):
-      cost = this.totalCost + this.medium;
-      break;
-    case ('large'):
-      cost = this.totalCost + this.large;
-      break;    
-  };
-  return cost;
-};
-Menu.prototype.setSizePrice = function(size) { 
-  size === 'small' ? this.totalCost = this.totalCost + this.small:
-  size === 'medium' ? this.totalCost = this.totalCost + this.medium:
-  size === 'large' ? this.totalCost = this.totalCost + this.large: 
-  null;
-};
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,101 +1,44 @@
 import {Menu} from './classes/Menu.js';
 
-const menuComponent = (obj, i) => {
-  return `
-    <div key={${i}} class="menu-component">
-      <div class="menu-name">
-        <p class="title">${obj[0]} ${obj[1]}${obj[2]}</p>
-        <div class="menu-description">${obj[3]}</div>
-        <div class="button-container">
-          <button id="${obj[6]}">Add ${obj[0]}</button>
-        </div>
-        <div class="item-totals">
-          <p key={${i}} class="items">${obj[5].total}</p>
-        </div>
-      </div>
-    </div>
-  `
-};
-const displayTotal = (obj) => {
-  return`
-    <div id="total">Total Cost With Us Today: $${obj.displayCurrentPrice()}</div>
-  `
-};
-const activateButton = (buttons, activeButton) => {
-  buttons.forEach(
-    button =>  button.style.boxShadow = '1px 0px 2px 0px black, -1px 0px 2px 0px black, inset -1px 0px 4px 0px rgba(0, 0, 0, 0.5), inset 1px 0px 4px 0px rgba(0, 0, 0, 0.5)'
-  );
-  activeButton.style.boxShadow = '0px 0px 2px 3px black';
-};
-const eventLogger = (event, thisArg, nodeList, totalModal, buttons) => {
-  const eventTarget = event.target.id;
-  switch (eventTarget) {
+
+
+const eventLogger = (event, thisArg) => {
+  const {id} = event.target
+  switch (id) {
     case ('parmigiano-button'):
-      thisArg.setParmigianoPrice(nodeList[0]);
+      thisArg.setParmigianoPrice();
       break;
     case ('paggiaro-button'):
-      thisArg.setPaggiaroPrice(nodeList[1]);
+      thisArg.setPaggiaroPrice();
       break;
     case ('brie-button'):
-      thisArg.setBriePrice(nodeList[2]);
+      thisArg.setBriePrice();
       break;
     case ('onion-button'):
-      thisArg.setOnionPrice(nodeList[3]);
+      thisArg.setOnionPrice();
       break;
     case ('olive-button'):
-      thisArg.setOlivePrice(nodeList[4]);
+      thisArg.setOlivePrice();
       break;
     case ('tomato-button'):
-      thisArg.setTomatoPrice(nodeList[5]);
+      thisArg.setTomatoPrice();
       break;
     case ('mushroom-button'):
-      thisArg.setMushroomPrice(nodeList[6]);
-      break;
-    case ('small'):
-      thisArg.setSize('small');
-      activateButton(buttons, event.target)
-      break;
-    case ('medium'):
-      thisArg.setSize('medium');
-      activateButton(buttons, event.target)
-      break;
-    case ('large'):
-      thisArg.setSize('large');
-      activateButton(buttons, event.target)
+      thisArg.setMushroomPrice();
       break;
     case('submit'):
       thisArg.setSizePrice();
-      totalModal.style.display = 'flex';
   };
 };
 document.addEventListener('DOMContentLoaded', () => {
-  const menuSelector = document.getElementById('menu-selector-root');
   const totalCost = document.querySelectorAll('.total-root');
   const totalModal = document.getElementById('total-popup');
-  const smallButton = document.getElementById('small');
-  const mediumButton = document.getElementById('medium');
-  const largeButton = document.getElementById('large');
-  const sizeButtons = [smallButton, mediumButton, largeButton];
 
   let menu = new Menu();
-  const myMenuItems = [
-    menu.parmigiano, 
-    menu.paggiaro,
-    menu.brie,
-    menu.onion,
-    menu.olive,
-    menu.tomato,
-    menu.mushroom
-  ];
-  totalCost.forEach(node => node.innerHTML = displayTotal(menu));
-  const menuMapper = myMenuItems.map(
-    (item, index) => {
-      return menuComponent(item, index)
-  }).join('');
-  menuSelector.innerHTML = menuMapper
+
   const toppingTotals = document.querySelectorAll('.items');
   document.getElementById('pizza-shop').addEventListener('click', event => {
-    eventLogger(event, menu, toppingTotals, totalModal, sizeButtons);
+    eventLogger(event, menu, toppingTotals, totalModal);
     totalCost.forEach(node => node.innerHTML = displayTotal(menu));
   });
 });
